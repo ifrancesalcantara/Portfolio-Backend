bbf91b106346211ab91f21c9c199b0d6c8b0ad73
Iván Francés <ifrancesalcantara@gmail.com>
2020-06-28 18:17:28 +0200

    heroku cors v2

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const app = express()
 app.use(
   cors({
     credentials: true,
-    origin: [process.env.PUBLIC_DOMAIN/*, "https://paintapop.herokuapp.com/"*/]
+    origin: [process.env.PUBLIC_DOMAIN, "https://ifrances-portfolio.herokuapp.com"]
   })
 )
 
